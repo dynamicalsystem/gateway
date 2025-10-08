73ed6859f2ed6de897140af13ec3fad1f8674f20
Simon Horrobin <simon@horrobin.net>
2025-10-08 12:34:10 +0100

    reduce boot volume size to free tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -146,10 +146,11 @@ resource "oci_core_instance" "gateway_instance" {
     memory_in_gbs = 6
   }
 
+  # Max 20 for free tier
   source_details {
     source_type             = "image"
     source_id               = data.oci_core_images.ubuntu_images.images[0].id
-    boot_volume_size_in_gbs = "50"
+    boot_volume_size_in_gbs = "20"
   }
 
   create_vnic_details {
